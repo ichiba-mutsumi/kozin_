52f2c60c24c9653b1adcbb1d102665107c5c3858
ichiba <urata.9489@gmail.com>
2022-10-26 14:09:18 +0900

    追加

--- musicgame/MusicGame-alpha--main/Project/GameProject/Game/ShareNum.cpp
+++
@@ -1,8 +0,0 @@
-#include "ShareNum.h"
-
-int ShareNum::GameNum = 0;
-int ShareNum::MaxGame = 3;
-int ShareNum::score = 0;
-int ShareNum::HitCount = 0;
-bool ShareNum::debug = false;
-bool ShareNum::Hit = false;
